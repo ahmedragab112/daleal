95ccc9b87c8ffd5a55a9d14fe6c9b4390c579b58
ahmedragab <ar1755@fayoum.edu.eg>
2024-02-14 00:32:30 +0200

    adding screen utils

--- lib/app/daleal.dart
+++ lib/app/daleal.dart
@@ -1,19 +1,44 @@
 import 'package:daleal/config/router/app_router.dart';
 import 'package:daleal/config/router/app_routes.dart';
+import 'package:daleal/config/theme/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class Daleal extends StatelessWidget {
   const Daleal({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_, child) {
+        return MaterialApp(
+          locale: const Locale('en'),
+          themeMode: ThemeMode.light,
+          theme: AppTheme.lightTheme,
+          darkTheme: AppTheme.darkTheme,
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
+          supportedLocales: AppLocalizations.supportedLocales,
+          debugShowCheckedModeBanner: false,
+          initialRoute: AppRoutes.loginPage,
+          onGenerateRoute: AppRouter.onGenerateRoutes,
+        );
+      },
+    );
+  }
+}
+
+
+/*MaterialApp(
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
       debugShowCheckedModeBanner: false,
       initialRoute: AppRoutes.loginPage,
       onGenerateRoute: AppRouter.onGenerateRoutes,
     );
-  }
-}
+  */
