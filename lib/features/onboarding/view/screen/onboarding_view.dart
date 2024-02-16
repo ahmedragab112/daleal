91d51ead1fafed4e8bae2bad94738eebd5283a8b
ahmedragab <ar1755@fayoum.edu.eg>
2024-02-16 04:07:32 +0200

    finsih onboarding screen

--- lib/features/onboarding/view/screen/onboarding_view.dart
+++ lib/features/onboarding/view/screen/onboarding_view.dart
@@ -1,27 +1,38 @@
 import 'package:daleal/core/utils/spaceing/spaceing.dart';
-import 'package:daleal/core/utils/widget/custom_btn.dart';
 import 'package:daleal/features/onboarding/view/widget/onboarding_body.dart';
 import 'package:daleal/features/onboarding/view/widget/skip_btn.dart';
 import 'package:flutter/material.dart';
 
-class OnBoardingView extends StatelessWidget {
+class OnBoardingView extends StatefulWidget {
   const OnBoardingView({super.key});
 
+  @override
+  State<OnBoardingView> createState() => _OnBoardingViewState();
+}
+
+class _OnBoardingViewState extends State<OnBoardingView> {
+  final PageController pageController = PageController(initialPage: 0);
+  int index = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Column(
-          children: [
-            const VerticalSpace(40)
- ,            const SkipButton(),
-            const VerticalSpace(32),
-            OnBoardingBody(
-              controller: PageController(),
+        child: CustomScrollView(
+          physics: const BouncingScrollPhysics(),
+          slivers: [
+            const SliverToBoxAdapter(child: VerticalSpace(40)),
+            const SliverToBoxAdapter(child: SkipButton()),
+            const SliverToBoxAdapter(child: VerticalSpace(32)),
+            SliverToBoxAdapter(
+              child: OnBoardingBody(
+                controller: pageController,
+                onPageChanged: (index) {
+                  this.index = index;
+                  setState(() {});
+                }, index: index,
+              ),
             ),
-            const CustomButton(),
-            const VerticalSpace(18)
           ],
         ),
       ),
