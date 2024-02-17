c9a8c910fa80e753d98d056895803257e9656228
ahmedragab <ar1755@fayoum.edu.eg>
2024-02-17 18:08:19 +0200

    init firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:daleal/app/daleal.dart';
 import 'package:daleal/core/cache/cache_helper.dart';
 import 'package:daleal/core/di/service_locator.dart';
+import 'package:daleal/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   setupLocator();
-  await locator<CacheHelper>().init();
+  await Future.wait([
+    Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    ),
+    locator<CacheHelper>().init()
+  ]);
+
   runApp(const Daleal());
 }
